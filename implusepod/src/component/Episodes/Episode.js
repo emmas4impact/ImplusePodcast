1fe0490eec6ac480adea7541ce9d1ad2b389c1ff
Emmanuel Adedeji <emmans4destiny@gmail.com>
2021-03-12 13:28:56 +0200

    Feature and episode added

---
+++ implusepod/src/component/Episodes/Episode.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Auxy from "../../hoc/Auxy";
+import NavBar from "../NavBar/NavBar";
+
+const Episode = (props) => {
+  return (
+    <Auxy>
+      <NavBar />
+      <div>Hi I am fetures</div>
+    </Auxy>
+  );
+};
+export default Episode;
---
+++ implusepod/src/component/Features/Feature.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Auxy from "../../hoc/Auxy";
+import NavBar from "../NavBar/NavBar";
+
+const Feature = (props) => {
+  return (
+    <Auxy>
+      <NavBar />
+      <div>Hi I am fetures</div>
+    </Auxy>
+  );
+};
+export default Feature;
