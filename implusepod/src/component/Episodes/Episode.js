cc96d700ab8f7ab0f8422295b478033ea90b837b
Emmanuel Adedeji <emmans4destiny@gmail.com>
2021-03-30 10:21:19 +0300

    audio file added

--- implusepod/src/component/Episodes/Episode.js
+++ implusepod/src/component/Episodes/Episode.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Auxy from "../../hoc/Auxy";
-
+import ReactAudioPlayer from "react-audio-player";
+import { Card } from "react-bootstrap";
 
 const Episode = (props) => {
   return (
     <Auxy>
       {/* <NavBar /> */}
-      <div>Hi I am Episodes</div>
+      <Card style={{ width: "22rem", marginTop:"20px"}}>
+        <Card.Body>
+          <Card.Title>First Episode</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            Introduction
+          </Card.Subtitle>
+          <Card.Text>
+            Short introduction about me and what I do.
+          </Card.Text>
+          <ReactAudioPlayer src="adeseyebanmeke.mp4" autoPlay controls />
+        </Card.Body>
+      </Card>
+
+     
     </Auxy>
   );
 };
